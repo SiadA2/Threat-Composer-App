d7c74a2a05c4ebc4e5d65fc1181be34df7d42caa
siadgit <siadadam2@gmail.com>
2025-09-21 09:43:29 +0100

    part 1

---
+++ Terraform/main.tf
@@ -0,0 +1,3 @@
+module "subnets" {
+    source = "./modules/subnets"
+}
---
+++ Terraform/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.14.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-siad"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+
